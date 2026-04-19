1556e987287e8ef53cbd58ed5d8658723c7bda8f
AniketT0008 <aniketthakur20008@gmail.com>
2026-04-19 01:36:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from maze_generator import generate_perfect_maze
 from visualizer import run_visual
 import sys
-from BFS_Breath_First_Search import solve_maze
-from DFS_Depth_First_Search import solve_maze
+from BFS_Breath_First_Search import solve_maze_BFS
+from DFS_Depth_First_Search import solve_maze_DFS
 sys.setrecursionlimit(10000)
 
 
